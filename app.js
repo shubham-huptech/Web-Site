1ee7060749384af4b93446176739c449226dd5d4
shubhhuptech <huptechdev8@gmail.com>
2022-11-02 17:43:10 +0530

    add js file

---
+++ app.js
@@ -0,0 +1,35 @@
+$("#popular-products").owlCarousel({
+  responsive: {
+    0: {
+      items: 1,
+    },
+    550: {
+      items: 2,
+    },
+    800: {
+      items: 3,
+    },
+    1100: {
+      items: 4,
+    },
+  },
+});
+
+$("#explore").owlCarousel({
+  loop: true,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    500: {
+      items: 2,
+    },
+    900: {
+      items: 3,
+    },
+    1100: {
+      items: 3,
+    },
+  },
+});
